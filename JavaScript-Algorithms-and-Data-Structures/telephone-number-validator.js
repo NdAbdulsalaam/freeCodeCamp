81bb9e2a8f0ca93251209a79f1c30f75107805fc
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-11-28 08:38:47 +0100

    re-write regex. Use '|' for the second group

--- JavaScript-Algorithms-and-Data-Structures/telephone-number-validator.js
+++ JavaScript-Algorithms-and-Data-Structures/telephone-number-validator.js
@@ -1,20 +1,28 @@
-function telephoneCheck(str) {
-  let myRegex = /(1\s?)?\(?\d{3}[) -]*?\d{3}[( -]?\d{4}$/;
+// function telephoneCheck(str) {
+//   let myRegex = /(1\s?)?\(?\d{3}[) -]*?\d{3}[( -]?\d{4}$/;
   
-  let myMatch = str.match(myRegex);
-  console.log(myMatch);
-    if (myMatch != null && myMatch[0] == str ){
-      let count = 0;
-      for (let i of myMatch[0]){
-        if (i == "(" || i == ")"){
-          console.log(count)
-          count ++;
-          console.log(count)
-        }
+//   let myMatch = str.match(myRegex);
+//   console.log(myMatch);
+//     if (myMatch != null && myMatch[0] == str ){
+//       let count = 0;
+//       for (let i of myMatch[0]){
+//         if (i == "(" || i == ")"){
+//           console.log(count)
+//           count ++;
+//           console.log(count)
+//         }
         
-      }
-      return count == 1? false: true;;
-    } else {
-      return false;
-    }
-}
+//       }
+//       return count == 1? false: true;;
+//     } else {
+//       return false;
+//     }
+// }
+
+function telephoneCheck(str) {
+    let myCheck = /^(1\s?)?(\(\d{3}\)|\d{3})([-\s]?)\d{3}([-\s]?)\d{4}$/;
+  
+    return myCheck.test(str);
+  }
+  
+  console.log(telephoneCheck("1 (555) 555-5555"));
