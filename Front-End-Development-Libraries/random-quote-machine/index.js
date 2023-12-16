0c2bf257f8d805e8a4670a78de955bea1ef71ef3
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-12-16 09:53:38 +0100

    change the href of timbir to that of linkedin

--- Front-End-Development-Libraries/random-quote-machine/index.js
+++ Front-End-Development-Libraries/random-quote-machine/index.js
@@ -60,9 +60,9 @@ function getQuote() {
       encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
   );
 
-  $('#tumblr-quote').attr(
+  $('#linkedin-quote').attr(
     'href',
-    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+    'https://www.linkedin.com/shareArticle?url=your-quote-url&title=Your%20Quote&summary=Your%20Quote%20Caption=' +
       encodeURIComponent(currentAuthor) +
       '&content=' +
       encodeURIComponent(currentQuote) +
