8ed0f9456181e92d70582e22eb9b61629a679cb5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 22:15:36 +0100

    create sub-dropdown for services

--- Front-End-Development-Libraries/todo-list/src/components/Navbar.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/Navbar.jsx
@@ -5,9 +5,21 @@ const Navbar = () => {
     console.log(useState(false));
 
     return (
-        <>
-            nav bar content
-        </>
+        <nav>
+            <ul>
+                <li>Home</li> 
+                <li>About</li>
+                <li>
+                    <button>
+                        Services <span>&#8595;</span>
+                    </button>
+                    <ul>
+                        <li>Design</li>
+                        <li>Development</li>
+                    </ul>
+                </li>
+           </ul>
+        </nav>
     )
 }
 
