ad72dd60712fd27f6f0ab6d342bdb636c4ade8e5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 09:11:30 +0100

    Update changes

---
+++ Front-End-Development-Libraries/todo-list/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <React.Fragment>
+            <h1>Todos</h1>
+            <p>items will be stored in the broser local disk</p>
+        </React.Fragment>
+    );
+    };
+
+export default Header
