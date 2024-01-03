03b47fc689a9b5a24ec6c02bfcdce82583e5d4dc
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 23:04:36 +0100

    Listen to click from outside when the dropdown is true and close it

--- Front-End-Development-Libraries/todo-list/src/components/Navbar.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/Navbar.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Navbar = () => {
-    const [dropdown, setDropdown] = useState(false)
+    const [dropdown, setDropdown] = useState(false);
+    const ref = useRef();
 
     const handleClick = () => {
         return setDropdown((prev) => !prev)
     }
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (dropdown && ref.current && !ref.current.contains(event.target)) {
+                setDropdown(!dropdown);
+            }
+        };
+        document.addEventListener("mousedown", handler);
+    }, [dropdown])
 
     return (
         <nav>
             <ul>
                 <li>Home</li> 
                 <li>About</li>
-                <li>
+                <li ref={ref}>
                     <button onClick={handleClick}>
                         Services <span>&#8595;</span>
                     </button>
