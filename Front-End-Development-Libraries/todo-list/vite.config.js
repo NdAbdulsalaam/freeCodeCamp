0eef6e96dca37e9d9b0a6869daabda208fec9995
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 08:28:20 +0100

    Update changes

--- Front-End-Development-Libraries/todo-list/vite.config.js
+++ Front-End-Development-Libraries/todo-list/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
-import path from 'path';
+
 export default defineConfig({
   resolve: {
     alias: {
