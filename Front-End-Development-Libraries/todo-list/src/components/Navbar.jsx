9719628587920cb8098288caaa8fed3681a758e7
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 21:49:16 +0100

    Create Navbar for my todos app

---
+++ Front-End-Development-Libraries/todo-list/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Navbar = () => {
+    console.log(useState(false));
+
+    return (
+        <>
+            nav bar content
+        </>
+    )
+}
+
+export default Navbar;
--- Front-End-Development-Libraries/todo-list/src/components/TodoApp.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/TodoApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '@/components/Header'
 import TodosLogic from '@/components/TodosLogic'
+import Navbar from '@/components/Navbar';
 
 const TodoApp = () => {
     return (
         // We can use "<> </>" instead of <React.Fragment> </React.Fragment>
         <>
+        <Navbar />
         <Header />
         <TodosLogic />
         </>
