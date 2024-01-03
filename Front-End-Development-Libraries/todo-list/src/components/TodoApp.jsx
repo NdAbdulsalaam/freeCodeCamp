f8d6cef01bc795d6afaefd2028450b1b68b57727
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-03 08:47:13 +0100

    Update changes

--- Front-End-Development-Libraries/todo-list/src/components/TodoApp.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/TodoApp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const TodoApp = () => {
     return (
+        // We can use "<> </>" instead
         <React.Fragment>
             <h1>Hello world!</h1>
             <p>I am in a React Component!</p>
