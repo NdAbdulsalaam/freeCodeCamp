4e35614ca0254cea8621555546b212719b3008c4
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-04 16:48:12 +0100

    Update changes

--- Front-End-Development-Libraries/todo-list/src/components/ContactApp.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/ContactApp.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 
 const ContactApp = () => {
     const [fname, setfname] = useState('')
+    const handleChange = (e) =>{
+        setfname(e.target.value);
+    };
 
     return (
         <>
             <h1>Contact Form</h1>
             <form>
-                <label>Firstname:
-                    <input type="text" value={fname} />
+                <label>First name:
+                    <input type="text" value={fname} onChange={handleChange} />
                 </label>
             </form>
             <h3>First name: {fname}</h3>
