4fd38f73765052dad8cc3592c362ffddfa4a0f4c
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-04 16:35:48 +0100

    Create a ContactApp for practice

--- Front-End-Development-Libraries/todo-list/src/main.jsx
+++ Front-End-Development-Libraries/todo-list/src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import TodoApp from "@/components/TodoApp";
+import ContactApp from "./ContactApp";
 
 const domContainer = document.getElementById("root")
 const root = ReactDOM.createRoot(domContainer)
 
 root.render(
   <React.StrictMode>
+    <ContactApp />
     <TodoApp />
   </React.StrictMode>
 )
