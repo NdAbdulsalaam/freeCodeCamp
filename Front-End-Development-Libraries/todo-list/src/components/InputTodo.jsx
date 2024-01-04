3e64ce015fff836549afb1bd535a24bdf76d0992
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-04 23:16:11 +0100

    Control react input

--- Front-End-Development-Libraries/todo-list/src/components/InputTodo.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const InputTodo = () => {
+
+        const [todo, setTodo] = useState('')
+
+        const handleChange = (e) => {
+            setTodo(e.target.value);
+        };
+
     return(
         <>
             <form>
-                <input type="text" placeholder="Add Todo..." />
-                
+                <input type="text" value={todo} placeholder="Add Todo..." onChange={handleChange} />
+
                 <button>Submit</button>
             </form>
         </>
