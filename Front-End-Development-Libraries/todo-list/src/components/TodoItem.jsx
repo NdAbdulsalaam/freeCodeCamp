a36d2ea515656dcc268a2ab1b721b9b9851df090
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-05 01:24:55 +0100

    Add dynamic styling to completed tasks using sass

--- Front-End-Development-Libraries/todo-list/src/components/TodoItem.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/TodoItem.jsx
@@ -1,12 +1,22 @@
 import styles from '@/styles/TodoItem.module.css';
 
 const TodoItem = ({ ItemProps, handleChange, delTodo }) => {
+
+    const completedStyle = {
+        fontStyle: 'italic',
+        color: '#595959',
+        opacity: 0.4,
+        textDecoration: 'line-through',
+      };
+
     return(
         <li className={styles.item}>
             <div className={styles.content}>
                 <input type="checkbox" checked={ItemProps.completed}
                 onChange={() =>handleChange(ItemProps.id)} />
-                {ItemProps.title}
+                 <span style={ItemProps.completed ? completedStyle : null}>
+                    {ItemProps.title}
+                </span>
                 <button onClick={() => delTodo(ItemProps.id)}>Delete</button>
             </div>
         </li>
