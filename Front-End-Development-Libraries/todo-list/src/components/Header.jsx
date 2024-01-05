69dd4bdbda180a5fe1627ce4a987574501420529
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-05 01:15:27 +0100

    Update changes

--- Front-End-Development-Libraries/todo-list/src/components/Header.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import styles from '@/styles/Header.module.css';
+
 const Header = () => {
     return(
-        <>
+        <header className={styles.header}>
             <h1>Todos</h1>
             <p>Items will be stored on the browser local disk</p>
-        </>
+        </header>
     );
     };
 
--- Front-End-Development-Libraries/todo-list/src/components/TodoItem.jsx
+++ Front-End-Development-Libraries/todo-list/src/components/TodoItem.jsx
@@ -1,13 +1,15 @@
+import styles from '@/styles/TodoItem.module.css';
+
 const TodoItem = ({ ItemProps, handleChange, delTodo }) => {
     return(
-        <>
-            <li>
+        <li className={styles.item}>
+            <div className={styles.content}>
                 <input type="checkbox" checked={ItemProps.completed}
                 onChange={() =>handleChange(ItemProps.id)} />
                 {ItemProps.title}
                 <button onClick={() => delTodo(ItemProps.id)}>Delete</button>
-            </li>
-        </>
+            </div>
+        </li>
     );
 };
 
