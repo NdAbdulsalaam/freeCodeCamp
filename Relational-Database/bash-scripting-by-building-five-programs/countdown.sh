7ffaf5d4db71adca22215a6b9700fabfae2a05a8
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-06 14:02:20 +0100

    Update countdown usinf 'for loop'

--- Relational-Database/bash-scripting-by-building-five-programs/countdown.sh
+++ Relational-Database/bash-scripting-by-building-five-programs/countdown.sh
@@ -2,9 +2,15 @@
 
 #Program that counts down to zero from a given argument
 
-if [[ $1 == arg1 ]]
+echo -e "\n~~ Countdown Timer ~~\n"
+
+if [[ $1 -gt 0 ]]
 then
-  echo true
+  for (( i = $1; i >= 0; i -- ))
+  do
+    echo $i
+    sleep 1
+  done
 else
-  echo false
+  echo "Include a positive integer as the first argument."
 fi
