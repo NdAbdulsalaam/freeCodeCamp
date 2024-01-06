89601986ebaf6b9da352b86347374ea4fbfbb5dd
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-06 14:21:35 +0100

    Update countdown using 'while loop'

--- Relational-Database/bash-scripting-by-building-five-programs/countdown.sh
+++ Relational-Database/bash-scripting-by-building-five-programs/countdown.sh
@@ -6,11 +6,22 @@ echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1 -gt 0 ]]
 then
-  for (( i = $1; i >= 0; i -- ))
-  do
-    echo $i
-    sleep 1
-  done
+
+    : ' Add comment to use a while loop
+    for (( i = $1; i >= 0; i -- ))
+    do
+        echo $i
+        sleep 1
+    done
+    '
+    I=$1
+    while [[ I -ge 0 ]]
+    do
+        echo $I
+        (( I-- ))
+        sleep 1
+    done
+
 else
   echo "Include a positive integer as the first argument."
 fi
