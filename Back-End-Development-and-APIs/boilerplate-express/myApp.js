94eaa2ffa73d86f1d8bde58647ffc3e037f561c5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-08 13:38:07 +0100

    Write first routing with Express

--- Back-End-Development-and-APIs/boilerplate-express/myApp.js
+++ Back-End-Development-and-APIs/boilerplate-express/myApp.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let app = express();
 
+console.log("Hellow World")
 
+const myFunc = (req, res) => {
+    res.send("Hello Express");
+}
 
+const index = function(req, res) {
+    res.send("Comming")
+}
 
+app.get("/", myFunc)
+// app.get('/', index)
 
 
 
