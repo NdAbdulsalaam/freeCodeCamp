9d3fb9385a9fa93dcd9aeda9aaf939c8ce64df01
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-08 21:15:45 +0100

    Chain middlewares

--- Back-End-Development-and-APIs/boilerplate-express/myApp.js
+++ Back-End-Development-and-APIs/boilerplate-express/myApp.js
@@ -45,6 +45,14 @@ const simpleLogger = function (req, res, next) {
 }
 app.use('/json', simpleLogger)
 
+// 8
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+    }, (req, res) => {
+        res.json({"time": req.time});
+    }
+    )
 
 
 
