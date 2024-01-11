0e1f7386a6843f23c38529bd4602a184d5688a59
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-11 17:10:39 +0100

    Update changes

--- Front-End-Development-Libraries/quote-machine/src/components/FetchQuote.jsx
+++ Front-End-Development-Libraries/quote-machine/src/components/FetchQuote.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import actionButton from '@/components/actionButton';
+import actionButton from '@/components/Button';
 
 function QuoteFetcher() {
   const [quoteData, setQuoteData] = useState({ quote: '', author: '' });
--- Front-End-Development-Libraries/quote-machine/src/components/QuoteApp.jsx
+++ Front-End-Development-Libraries/quote-machine/src/components/QuoteApp.jsx
@@ -1,61 +1,69 @@
 import React from "react";
-import "./styles.css"
 // import SocialMedia from "./Components/SocialMedia";
 import Button from "@/components/Button";
-import Owner from "@/components/Contributor";
+import Contributor from "@/components/Contributor";
+import FetchQuote from '@/components/FetchQuote';
 // import Title from "./Components/Title";
 
-export default function App() {
-  const [quotes, setQuotes] = React.useState([]);
-  const [randomQuote, setRandomQuote] = React.useState("");
-  const [color, setColor] = React.useState("#16a085'");
+// const QuoteApp = function App() {
+//   const [quotes, setQuotes] = React.useState([]);
+//   const [randomQuote, setRandomQuote] = React.useState("");
+//   const [color, setColor] = React.useState("#16a085'");
 
-  React.useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("https://type.fit/api/quotes");
-      const data = await response.json();
-      setQuotes(data);
-      let randIndex = Math.floor(Math.random() * data.length);
-      setRandomQuote(data[randIndex]);
-    }
-    fetchData();
-  }, []);
+//   React.useEffect(() => {
+//     async function fetchData() {
+//       const response = await fetch("https://type.fit/api/quotes");
+//       const data = await response.json();
+//       setQuotes(data);
+//       let randIndex = Math.floor(Math.random() * data.length);
+//       setRandomQuote(data[randIndex]);
+//     }
+//     fetchData();
+//   }, []);
 
-  const getNewQuote = () => {
-    const colors = [ '#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964',
-                      '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
-    let randIndex = Math.floor(Math.random() * quotes.length);
-    let randColorIndex = Math.floor(Math.random() * colors.length);
-    setRandomQuote(quotes[randIndex]);
-    setColor(colors[randColorIndex]);
-  };
+//   const getNewQuote = () => {
+//     const colors = [ '#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6',
+//                     '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
+//     let randIndex = Math.floor(Math.random() * quotes.length);
+//     let randColorIndex = Math.floor(Math.random() * colors.length);
+//     setRandomQuote(quotes[randIndex]);
+//     setColor(colors[randColorIndex]);
+//   };
 
-  return (
-    <div style={{ backgroundColor: color, minHeight: "100vh" }}>
-      <div className="container pt-5 custom-container">
-        <div className="jumbotron" id="quote-box">
-          <div className="card">
-            <Title/>
-            <div className="card-body text-center">
-              {randomQuote ? (
-                <>
-                  <p id="text" className="card-text">&quot;{randomQuote.text}&quot;</p>
-                  <h5 id="author" className="card-title mb-4">~ {randomQuote.author || "Anonymous"}</h5>
-                </>
-              ) : (
-                <h2>Loading...</h2>
-              )}
-                  <div className="flex">
-                    <SocialMedia quote={randomQuote}/>
-                    <Button action={getNewQuote}/>
-                  </div>
-            </div>
-          </div>
-          <Owner/>
-        </div>
-      </div>
-    </div>
-  );
+//   return (
+//     <div style={{ backgroundColor: color, minHeight: "100vh" }}>
+//       <div className="container pt-5 custom-container">
+//         <div className="jumbotron" id="quote-box">
+//           <div className="card">
+//             <Title/>
+//             <div className="card-body text-center">
+//               {randomQuote ? (
+//                 <>
+//                   <p id="text" className="card-text">&quot;{randomQuote.text}&quot;</p>
+//                   <h5 id="author" className="card-title mb-4">~ {randomQuote.author || "Anonymous"}</h5>
+//                 </>
+//               ) : (
+//                 <h2>Loading...</h2>
+//               )}
+//                   <div className="flex">
+//                     <SocialMedia quote={randomQuote}/>
+//                     <Button action={getNewQuote}/>
+//                   </div>
+//             </div>
+//           </div>
+//           <Owner/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+const QuoteApp = function() {
+  return(
+    <>
+      <FetchQuote />
+    </>
+  )
 }
 
-
+export default QuoteApp;
