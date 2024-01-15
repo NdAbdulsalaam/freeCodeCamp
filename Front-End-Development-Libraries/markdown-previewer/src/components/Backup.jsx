b0e9205223001ea90e7f4b5771dbb8fb7c692ceb
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-15 10:11:00 +0100

    Backup and redo

---
+++ Front-End-Development-Libraries/markdown-previewer/src/components/Backup.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { marked } from "marked";
+
+const MarkdownPreviewer = () => {
+  // ```
+  // // this is multi-line code:
+
+  // function anotherExample(firstLine, lastLine) {
+  //   if (firstLine == '```' && lastLine == '```') {
+  //     return multiLineCode;
+  //   }
+  // }
+  // ```\n\n" +
+  // `Wild Header | Crazy Header | Another Header?
+  // ------------ | ------------- | -------------
+  // Your content can | be here, and it | can be here....
+  // And here. | Okay. | I think we get it. \n\n" +`
+
+  const initalText = "# Welcome to my React Markdown Previewer! \n\n" +
+
+                    "## BY: Nurudeen Abdulsalaam \n\n" +
+
+                    "### And here's some other cool stuff:\n\n" +
+
+                    "Heres some code, `<div></div>`, between 2 backticks.\n\n" +
+                    ```
+                    // this is multi-line code:
+
+                    function anotherExample(firstLine, lastLine) {
+                      if (firstLine == ) {
+                        return multiLineCode;
+                      }
+                    }
+                    ``` +
+                   
+                    " You can also make text **bold**... whoa!\n\n" +
+                    "Or _italic_.\n\n" +
+                    "Or... wait for it... **_both!_**\n\n" +
+                    "And feel free to go crazy ~~crossing stuff out~~.\n\n" +
+
+                    "There's also [links](https://www.freecodecamp.org), and\n\n" +
+                    "> Block Quotes!\n\n" +
+
+                    "And if you want to get really crazy, even tables:\n\n" +
+
+                    "- And of course there are lists.\n\n" +
+                    "  - Some are bulleted.\n\n" +
+                    "    - With different indentation levels.\n\n" +
+                    "        - That look like this.\n\n" +
+
+
+                    "1. And there are numbered lists too.\n\n" +
+                    "1. Use just 1s if you want!\n\n" +
+                    "1. And last but not least, let's not forget embedded images:\n\n" +
+
+                    "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n\n" +
+                    "  **Happy Hacking!**"
+                  
+  const[markdown, setMardown] = useState(initalText)
+
+
+  return (
+    <div>
+
+      <h1>Hello</h1>
+        <div className="editor">
+          <textarea id="editor"></textarea>
+        </div>
+        <div className="preview">
+        <div id="preview" dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
+        </div>
+    </div>
+  )
+}
+
+export default MarkdownPreviewer;
--- Front-End-Development-Libraries/markdown-previewer/src/components/MarkdownPreviewer.jsx
+++ Front-End-Development-Libraries/markdown-previewer/src/components/MarkdownPreviewer.jsx
@@ -2,48 +2,30 @@ import React, { useState } from "react";
 import { marked } from "marked";
 
 const MarkdownPreviewer = () => {
-  // ```
-  // // this is multi-line code:
+  const initalText = `
+  # BY: Nurudeen Abdulsalaam
 
-  // function anotherExample(firstLine, lastLine) {
-  //   if (firstLine == '```' && lastLine == '```') {
-  //     return multiLineCode;
-  //   }
-  // }
-  // ```\n\n" +
-
-  const initalText = "# Welcome to my React Markdown Previewer! \n\n" +
-
-                    "## BY: Nurudeen Abdulsalaam \n\n" +
-
-                    "### And here's some other cool stuff:\n\n" +
-
-                    "Heres some code, `<div></div>`, between 2 backticks.\n\n" +
-
-                   
-                    " You can also make text **bold**... whoa!\n\n" +
-                    "Or _italic_.\n\n" +
-                    "Or... wait for it... **_both!_**\n\n" +
-                    "And feel free to go crazy ~~crossing stuff out~~.\n\n" +
-
-                    "There's also [links](https://www.freecodecamp.org), and\n\n" +
-                    "> Block Quotes!\n\n" +
-
-                    "And if you want to get really crazy, even tables:\n\n" +
-
-                    "- And of course there are lists.\n\n" +
-                    "  - Some are bulleted.\n\n" +
-                    "    - With different indentation levels.\n\n" +
-                    "        - That look like this.\n\n" +
-
-
-                    "1. And there are numbered lists too.\n\n" +
-                    "1. Use just 1s if you want!\n\n" +
-                    "1. And last but not least, let's not forget embedded images:\n\n" +
-
-                    "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n\n" +
-                    "  **Happy Hacking!**"
-                  
+  ## AS: freeCodeCamp [Project](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer)
+  
+  Inline code: \`const variable = "Hello World";\`
+  
+  Code block:
+  
+  \`\`\`The day is bright
+  function greet() {
+    console.log("Hello World!");
+  }
+  \`\`\`
+  
+  - List item 1
+  - List item 2
+  
+  > Blockquote: This is a blockquote.
+  
+  ![Markdown Image](../public/i-love-markdown.jpg)
+  
+  **Happy Hacking!**  
+  `
   const[markdown, setMardown] = useState(initalText)
 
 
