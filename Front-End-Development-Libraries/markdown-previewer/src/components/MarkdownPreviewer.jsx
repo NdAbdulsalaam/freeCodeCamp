7b326693b3b5d3a5f5638381e8e789296dcc2f38
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-15 09:09:28 +0100

    Add default text on initial rendering

--- Front-End-Development-Libraries/markdown-previewer/src/components/MarkdownPreviewer.jsx
+++ Front-End-Development-Libraries/markdown-previewer/src/components/MarkdownPreviewer.jsx
@@ -2,6 +2,33 @@ import React, { useState } from "react";
 import { Marked } from "marked";
 
 const MarkdownPreviewer = () => {
+  const initalText = `
+  # BY: Nurudeen Abdulsalaam
+
+  ## AS: freeCodeCamp Project
+  
+  [View Project](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer)
+  
+  Inline code: \`const variable = "Hello World";\`
+  
+  Code block:
+  
+  \`\`\`The day is bright
+  function greet() {
+    console.log("Hello World!");
+  }
+  \`\`\`
+  
+  - List item 1
+  - List item 2
+  
+  > Blockquote: This is a blockquote.
+  
+  ![Markdown Image](../public/i-love-markdown.jpg)
+  
+  **Happy Hacking**  
+  `
+  const[markdown, setMardown] = useState('')
 
 
   return (
