f6bb832e9b11bf48cea839c4a4530e73c7ee48e6
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-16 17:42:42 +0100

    Create index.css as directed in the tailwind doc and update main.jsx as required

--- Front-End-Development-Libraries/markdown-previewer/src/main.jsx
+++ Front-End-Development-Libraries/markdown-previewer/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MarkdownPreviewer from "@/components/MarkdownPreviewer";
-import '@/styles.css'
+import '@/index.css'
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 
 root.render(
   <React.StrictMode>
+    <h1 className="text-3xl text-red-600 font-bold underline">
+      Hello world!
+    </h1>
     < MarkdownPreviewer />
   </React.StrictMode>
 );
